6a1f385ec4edef8657ade61340ef904950295afb
diego fernando silva estrada <diego61978117@gmail.com>
2024-04-11 23:19:52 -0400

    Proyecto realizado1232

---
+++ ColingRealizado/Coling.Vista.Prueba/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Coling.Vista.Prueba
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
